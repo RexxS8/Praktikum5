1031fc15974bb1bb7be92bc04a4e7b9b5ebc0f18
RexxS8 <andri4martin@gmail.com>
2022-10-19 23:19:47 +0700

    Create Praktikum 5 Latihan 2 Andri Martin.py

---
+++ Praktikum 5 Latihan 2 Andri Martin.py
@@ -0,0 +1,26 @@
+print("Program untuk menghitung tiket masuk kebun binatang berdasarkan umur beserta pembayarannya")
+umur= "0"
+total = 0
+while (umur != ""):
+    umur = input("Masukan Umur: ")
+    if umur != "":
+        umur_angka = int(umur)
+        if umur_angka <= 2 :
+            print("Gratis")
+            price = 0
+        elif umur_angka >= 3 and umur_angka <= 12 :
+            print("Harga $14.00")
+            price = 14
+        elif umur_angka >= 65 :
+            print("Harga $18.00")
+            price = 18
+        else:
+            print("Harga $23.00")
+            price = 23
+        total = total + price
+        print("Total: $ %0.2f" % total)
+
+jumlah = int(input("Masukan Jumlah Uang: "))
+hasil = jumlah - total
+print("Kembalian: $ %0.2f" % hasil)
+print("Terimakasih Telah Menggunakan Program")
